5e3286964c955358c0c502d200ba35738a7af53d
Christoph Bühler <cbuehler@rootd.ch>
2021-05-03 13:58:16 +0200

    feat: add zitadel project ID

--- lib/zitadel_defaults.dart
+++ lib/zitadel_defaults.dart
@@ -4,5 +4,8 @@ const ZitadelIssuer = 'https://issuer.zitadel.ch';
 /// Default API endpoint for public Zitadel.
 const ZitadelApiEntpoint = 'api.zitadel.ch';
 
+/// Project ID for the Zitadel API.
+const ZitadelApiProjectId = '69234237810729019';
+
 /// Default discovery endpoint for OIDC.
 const DiscoveryEndpointPath = '/.well-known/openid-configuration';
