019c4f6d19f2c606e6fd4921c346cb4607dd7ce7
Christoph Bühler <buehler@users.noreply.github.com>
2023-01-24 17:13:00 +0700

    fix: export all relevant types (#97)
    This creates barrel files in the api folder
    that export all relevant types from
    google and zitadel in their
    respective folders.
    
    BREAKING CHANGE: This moves the admin, auth and management
    files into the ZITADEL subfolder to adhere to the new
    structure. All relevant types are now exported via the
    api folder (google well-known and zitadel types).

--- lib/api/management.dart
+++
@@ -1,6 +0,0 @@
-/// Exports all the management gRPC APIs.
-library management;
-
-export '../src/grpc/generated/zitadel/management.pb.dart';
-export '../src/grpc/generated/zitadel/management.pbenum.dart';
-export '../src/grpc/generated/zitadel/management.pbgrpc.dart';
--- test/clients_io_test.dart
+++ test/clients_io_test.dart
@@ -1,10 +1,10 @@
 @TestOn('vm')
 
 import 'package:test/test.dart';
-import 'package:zitadel/api/admin.dart' as admin;
-import 'package:zitadel/api/auth.dart' as auth;
+import 'package:zitadel/api/zitadel/admin.dart' as admin;
+import 'package:zitadel/api/zitadel/auth.dart' as auth;
 import 'package:zitadel/api/clients.dart';
-import 'package:zitadel/api/management.dart' as management;
+import 'package:zitadel/api/zitadel/management.dart' as management;
 
 import 'test_data.dart';
 
