49588cb8fe6d70e20a73f8f55ebe446df58fbdfa
Christoph Bühler <buehler@users.noreply.github.com>
2023-01-24 14:18:58 +0700

    fix: expose the grpc client types (#96)

--- lib/api/admin.dart
+++ lib/api/admin.dart
@@ -3,3 +3,4 @@ library admin;
 
 export '../src/grpc/generated/zitadel/admin.pb.dart';
 export '../src/grpc/generated/zitadel/admin.pbenum.dart';
+export '../src/grpc/generated/zitadel/admin.pbgrpc.dart';
--- lib/api/management.dart
+++ lib/api/management.dart
@@ -3,3 +3,4 @@ library management;
 
 export '../src/grpc/generated/zitadel/management.pb.dart';
 export '../src/grpc/generated/zitadel/management.pbenum.dart';
+export '../src/grpc/generated/zitadel/management.pbgrpc.dart';
